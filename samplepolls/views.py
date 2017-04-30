be6c75683734968d2af39e04c108df281d5929b5
jczetta <jczetta@gmail.com>
2017-04-30 17:55:32 -0400

    Fix complex view to use template

--- samplepolls/views.py
+++ samplepolls/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 from .models import Question
 
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output) # what is this returning?
-
-
+    template = loader.get_template('samplepolls/index.html')
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return HttpResponse(template.render(context,request))
+# Check out the docs for more explanation of this
 
 
 
